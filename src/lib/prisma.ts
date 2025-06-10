c638104606fa2dcc28589a76b27c0f871ddc3b63
Siddhant Jain <sidjain88tx@gmail.com>
2025-06-10 09:50:00 -0500

    create prisma connect file

---
+++ src/lib/prisma.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "../../generated/prisma";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+const prisma =
+  process.env.NODE_ENV === "production"
+    ? new PrismaClient()
+    : global.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
+
+export default prisma;
