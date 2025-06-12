408cc66b1f30640fc5add94ea6bc5bc61bb2f0ac
Siddhant Jain <sidjain88tx@gmail.com>
2025-06-11 23:51:44 -0500

    implement DELETE and PATCH routes for todos with user authorization and error handling

--- src/app/api/todo/[id]/route.ts
+++ src/app/api/todo/[id]/route.ts
@@ -4,8 +4,39 @@ import { auth } from "@clerk/nextjs/server";
 import prisma from "@/lib/prisma";
 import type { NextRequest } from "next/server";
 import type { ContextWithId } from "@/lib/withLoggerAndErrorHandler";
+import { TodoResponseData } from "@/types/todo";
 
 export const DELETE = withLoggerAndErrorHandler(
+  async (_: NextRequest, { params }: ContextWithId) => {
+    const { userId } = await auth();
+
+    if (!userId) {
+      return errorResponse("Unauthorized", 401);
+    }
+
+    const { id: todoId } = params;
+
+    const todo = await prisma.todo.findUnique({
+      where: {
+        id: todoId,
+      },
+    });
+
+    if (todo?.userId !== userId) {
+      return errorResponse("Unauthorized", 401);
+    }
+
+    await prisma.todo.delete({
+      where: {
+        id: todoId,
+      },
+    });
+
+    return successResponse("Todo deleted successfully", null, 204);
+  }
+);
+
+export const PATCH = withLoggerAndErrorHandler(
   async (request: NextRequest, { params }: ContextWithId) => {
     const { userId } = await auth();
 
@@ -13,8 +44,33 @@ export const DELETE = withLoggerAndErrorHandler(
       return errorResponse("Unauthorized", 401);
     }
 
-    const { id } = params;
+    const { isCompleted } = await request.json();
 
-    return successResponse("Todo deleted successfully", null);
+    if (typeof isCompleted !== "boolean") {
+      return errorResponse("Invalid request body", 400);
+    }
+
+    const { id: todoId } = params;
+
+    const todo = await prisma.todo.findUnique({
+      where: {
+        id: todoId,
+      },
+    });
+
+    if (todo?.userId !== userId) {
+      return errorResponse("Unauthorized", 401);
+    }
+
+    const updatedTodo = await prisma.todo.update({
+      where: { id: todoId },
+      data: { isCompleted },
+    });
+
+    return successResponse<TodoResponseData>(
+      "Todo updated successfully",
+      { todo: updatedTodo },
+      200
+    );
   }
 );
