7b06c62be8b6c0c535777885be1ea6b89bbc1b05
Siddhant Jain <sidjain88tx@gmail.com>
2025-06-12 00:46:27 -0500

    create admin route security

---
+++ src/app/api/admin/route.ts
@@ -0,0 +1,8 @@
+import { clerkClient } from "@clerk/nextjs/server";
+
+async function isAdmin(userId: string) {
+  const client = await clerkClient();
+  const user = await client.users.getUser(userId);
+
+  return user.publicMetadata.role === "admin";
+}
